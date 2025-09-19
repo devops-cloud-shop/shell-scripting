60b5a61a7c9bb990ebf770ca07d4d6a3bbe84744
Prav-Akula <pravs.akula@outlook.com>
2025-09-19 15:53:44 -0400

    passing values as arguments

---
+++ 02-variables-as-args.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+#passing values as arguments
+
+echo "$PERSON1:: Hi $PERSON2, How are you?"  # this is how you refer a variable in shell script.
+echo "$PERSON2:: Hello $PERSON1, I am fine. How are you?"
+echo "$PERSON1:: What are you learning recently?"
+echo "$PERSON2:: I am learning Shell scripting $PERSON1"
