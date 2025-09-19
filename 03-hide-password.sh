7096e105d9c53413cd995b9ca1703ec0b7198e29
Prav-Akula <pravs.akula@outlook.com>
2025-09-19 16:17:43 -0400

    passing hidden pin-number

--- 03-hide-password.sh
+++ 03-hide-password.sh
@@ -4,4 +4,4 @@ echo "Please enter your PIN Number"
 
 read -s PIN_NUMBER # Here PIN_NUMBER is a variable
 
-echo "Your PIN Number is $PIN_NUMBER"
+echo "Your PIN Number is $PIN_NUMBER" #This is to see what you have given as pin. it is only for practice.
