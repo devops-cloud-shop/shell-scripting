7baf01f41a985a523dbe63b96430ac7de3a8b9d8
Prav-Akula <pravs.akula@outlook.com>
2025-09-19 17:36:57 -0400

    datatypes

--- 08-datatypes.sh
+++ 08-datatypes.sh
@@ -6,3 +6,13 @@ NUMBER2=200
 SUM=$(($NUMBER1+$NUMBER2))
 
 echo "SUM is : $SUM"
+
+
+#List and using index
+
+# Size = 4, Max index = 3
+LEADERS=("Modi" "Putin" "Trudo" "Trump")
+
+echo "All leaders: ${LEADERS[@]}"
+echo "First Leader: ${LEADERS[0]}"
+echo "Second Leader: ${LEADERS[1]}"
