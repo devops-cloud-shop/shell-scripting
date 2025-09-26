86682a8879fe1ce4a6af03ee47402da31792ef13
Prav-Akula <pravs.akula@outlook.com>
2025-09-26 17:13:17 -0400

    colors

--- 13-colors.sh
+++ 13-colors.sh
@@ -3,6 +3,9 @@
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
+N="\e[0m"
 
-echo -e "Installing ... $R FAILURE"
+echo -e "Installing ... $R FAILURE $N"
+echo -e "Installation ... $G SUCCESS $N"
+echo -e "Package already exists ... $Y SKIPPING $N"
 echo "checking the colors"
