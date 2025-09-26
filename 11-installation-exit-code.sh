f51f99b7ac01152cd3762ed88c0ba3105f69e230
Prav-Akula <pravs.akula@outlook.com>
2025-09-26 13:30:06 -0400

    installation of packages only with root and exit codes

---
+++ 11-installation-exit-code.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]; then   #checking if the user has root access - root userid is 0
+
+    echo "ERROR:: Please run the script with root priviledges"
+    exit 1    #if the user is not a root user the script should exit and not proceed further- hence we write exit status
+
+fi
+
+dnf install mysql -y
+
+if [ $? -ne 0 ]; then
+
+    echo "ERROR:: Installating MySQL is failure"
+else
+    echo "Installation of MySQL is SUCCESS"
+fi
