781c551a43142db71c33e0b703354745d1e42643
Prav-Akula <pravs.akula@outlook.com>
2025-09-29 13:04:41 -0400

    set and trap command

--- 17-set.sh
+++ 17-set.sh
@@ -2,9 +2,11 @@
 
 set -e   #exit the script when error occurs
 
+trap 'echo "There is an error in $LINENO, Command is: $BASH_COMMAND"' ERR
+
 echo "Hello"
 echo "Before error"
-frtkd-scmd/; # 
+frtkd-scmd/; # Here shell script understands there is an error and signals as ERR
 echo "After error"
 
 
