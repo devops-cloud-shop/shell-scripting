52851ea1a9e3feec84d0ce22807cda45f481ae36
Prav-Akula <pravs.akula@outlook.com>
2025-09-30 12:52:26 -0400

    calling scripts

--- 18-script-1.sh
+++ 18-script-1.sh
@@ -4,9 +4,10 @@ NAME=INDIA
 
 echo "My county is $NAME"
 echo "PID of script-1: $$"
-sh 19-script-2.sh
+#sh 19-script-2.sh
 
 # here we are accessing script 2 - sh script-2.sh
 
-#source ./19-script-2.sh
+source ./19-script-2.sh # source command searches the file in the path, here it is current dir (.)
+
 
