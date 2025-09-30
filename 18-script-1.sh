23cb50551466f12077db577771e39fdc2038f55e
Prav-Akula <pravs.akula@outlook.com>
2025-09-30 18:48:18 -0400

    shell-script

--- 18-script-1.sh
+++ 18-script-1.sh
@@ -6,7 +6,7 @@ echo "My county is $NAME"
 echo "PID of script-1: $$"
 #sh 19-script-2.sh
 
-# here we are accessing script 2 - sh script-2.sh
+# here we are accessing script 2
 
 source ./19-script-2.sh # source command searches the file in the path, here it is current dir (.)
 
