bcde4e3c85ddd5f2b4b6c2974a5f8b423a3f32e3
Prav-Akula <pravs.akula@outlook.com>
2025-10-02 18:08:24 -0400

    Delete-old-logs

--- 20-delete-old-logs.sh
+++ 20-delete-old-logs.sh
@@ -6,7 +6,7 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-LOG_FOLDER="/var/log/shell-scripting"
+LOG_FOLDER="/var/log/shell-script"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME.log" #/var/log/shell-scripting/20-delete-old-logs.log
 
@@ -15,3 +15,15 @@ mkdir -p $LOG_FOLDER  # -p creates folder if not exists
 echo "Script started execution at : $(date)" | tee -a $LOG_FILE
 
 SOURCE_DIR=/home/ec2-user/shell-scripting/app-logs
+
+if [ ! -d $SOURCE_DIR ]; then
+    echo "ERROR:: $SOURCE_DIR does not exists"
+    exit 1
+fi
+
+FILES_TO_DELETE=$(find $SOURCE_DIR -name "*.log" -type f -mtime +14)
+
+while IFS= read -r filepath; 
+do
+    echo " Deleting the files: $filepath "
+done  <<< $FILES_TO_DELETE
