5793c1182b3d03eba1721ed7aad0b95cdaca3589
Prav-Akula <pravs.akula@outlook.com>
2025-10-04 17:27:37 -0400

    disk-usage

--- 23-disk-usage.sh
+++ 23-disk-usage.sh
@@ -1,10 +1,14 @@
 #!/bin/basah
 
 DISK_USAGE=$(df -hT | grep -v Filesystem)
+DISK_THRESHOLD=2 #In project we keep the value as 75-80
+MESSAGE=""
 
 while IFS= read -r line
 do
     USAGE=$(echo $line | awk '{print $6}' | cut -d "%" -f1)
     PARTITION=$( echo $line | awk '{print $7}')
+    if [ $USAGE -ge $DISK_THRESHOLD ]; then
+        MESSAGE+="High disk usage on $PARTITION: $USAGE %
  
 done <<< $DISK_USAGE
